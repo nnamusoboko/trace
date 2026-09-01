b1b965148456890ffd294ea43524148a17ef8b30
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-09-01 18:40:29 +0300

    feat: connect ai client

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+from openai.types.admin.organization import role
+
+_ = load_dotenv()
+api_key = os.environ.get("OPENROUTER_API_KEY")
+if not api_key:
+    raise RuntimeError("Empty api key")
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key
+)
+
 def main():
-    print("Hello from trace!")
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=[
+            {
+                "role": "user",
+                "content": "Describe an ai agent? Use one paragraph maximum."
+            }
+        ]
+    )
+    print(f"response: {response.choices[0].message.content}")
 
 
 if __name__ == "__main__":
