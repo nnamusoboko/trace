c7c8f02e07a7a8288e0aa3a379364a9c8e688aca
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-09-04 11:59:31 +0300

    feat: create function map and call function from llm

--- call_function.py
+++ call_function.py
@@ -1,8 +1,22 @@
-from openai.types.chat import ChatCompletionToolParam
-from functions.get_files_info import schema_get_files_info
-from functions.run_code import schema_run_python_file
-from functions.write_to_file import schema_write_file
-from functions.get_file_content import schema_get_file_content
+import json
+from collections.abc import Callable
+from typing import TypedDict
+
+from openai.types.chat import (
+    ChatCompletionMessageFunctionToolCall,
+    ChatCompletionToolParam,
+)
+
+from functions.get_file_content import get_file_content, schema_get_file_content
+from functions.get_files_info import get_files_info, schema_get_files_info
+from functions.run_code import run_python_file, schema_run_python_file
+from functions.write_to_file import schema_write_file, write_file
+
+
+class ToolCallResultMessage(TypedDict):
+    role: str
+    tool_call_id: str
+    content: str
 
 
 available_functions: list[ChatCompletionToolParam] = [
@@ -11,3 +25,35 @@ available_functions: list[ChatCompletionToolParam] = [
     schema_write_file,
     schema_get_file_content,
 ]
+
+function_map: dict[str, Callable] = {
+    "get_files_info": get_files_info,
+    "run_python_file": run_python_file,
+    "write_file": write_file,
+    "get_file_content": get_file_content
+}
+
+def call_function(tool_call: ChatCompletionMessageFunctionToolCall, verbose: bool = False) -> ToolCallResultMessage:
+    function_name = tool_call.function.name
+    function_args = json.loads(tool_call.function.arguments or "{}")
+
+    if verbose:
+        print(f" - Calling function: {function_name}({function_args})")
+    else:
+        print(f" - Calling function: {function_name}")
+
+    if function_name not in function_map:
+        return {
+            "role": "tool",
+            "tool_call_id": tool_call.id,
+            "content": f"Error: Unknown function: {function_name}",
+        }
+
+    function_args["working_directory"] = "./calculator"
+
+    result = function_map[function_name](**function_args)
+    return {
+        "role": "tool",
+        "tool_call_id": tool_call.id,
+        "content": result,
+    }
