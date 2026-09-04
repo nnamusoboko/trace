ece9167492b42db18fcb0c05db4e875cb033bcf2
Namusoboko Nelson <nnamusoboko@gmail.com>
2026-09-04 13:01:18 +0300

    refactor: use official ChatCompletionToolMessageParam type for function return value

--- call_function.py
+++ call_function.py
@@ -1,23 +1,17 @@
 import json
 from collections.abc import Callable
-from typing import TypedDict
 
 from openai.types.chat import (
     ChatCompletionMessageFunctionToolCall,
     ChatCompletionToolParam,
+    ChatCompletionToolMessageParam
 )
 
+import config
 from functions.get_file_content import get_file_content, schema_get_file_content
 from functions.get_files_info import get_files_info, schema_get_files_info
 from functions.run_code import run_python_file, schema_run_python_file
 from functions.write_to_file import schema_write_file, write_file
-import config
-
-class ToolCallResultMessage(TypedDict):
-    role: str
-    tool_call_id: str
-    content: str
-
 
 available_functions: list[ChatCompletionToolParam] = [
     schema_get_files_info,
@@ -33,7 +27,7 @@ function_map: dict[str, Callable] = {
     "get_file_content": get_file_content
 }
 
-def call_function(tool_call: ChatCompletionMessageFunctionToolCall, verbose: bool = False) -> ToolCallResultMessage:
+def call_function(tool_call: ChatCompletionMessageFunctionToolCall, verbose: bool = False) -> ChatCompletionToolMessageParam:
     function_name = tool_call.function.name
     function_args = json.loads(tool_call.function.arguments or "{}")
 
